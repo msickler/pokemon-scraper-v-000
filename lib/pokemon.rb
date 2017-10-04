8d7526816a6b04a3a6cd281ec3b938027bd44dab
Mariana Sickler <marianaleal_93@hotmail.com>
2017-10-04 15:02:20 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,7 +1,7 @@
 class Pokemon
   attr_reader :id, :name, :type, :db
   @@all = []
-  
+
   def initialize(id, name, type, db)
     @id = id
     @name = name
@@ -9,6 +9,7 @@ class Pokemon
     @db = db
     @@all << self
   end
+  
   def self.save(name, type, db)
     db.execute("INSERT INTO pokemon (name, type) VALUES (?, ?)", name, type)
   end
